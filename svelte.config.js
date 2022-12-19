103266da512854687bac3a8fa9c51013dde4e2fd
MatsDK <67562518+MatsDK@users.noreply.github.com>
2022-12-19 23:15:19 +0100

    add tailwindcss

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,15 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+	preprocess: [
+		vitePreprocess(),
+		preprocess({
+			postcss: true
+		})
+	],
 	kit: {
 		adapter: adapter()
 	}
