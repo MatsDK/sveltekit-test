da06e2636c07487ba7e68f3843cfc91cb1701283
MatsDK <67562518+MatsDK@users.noreply.github.com>
2023-03-18 22:04:37 +0100

    init chrome extension with vite

---
+++ extension/postcss.config.cjs
@@ -0,0 +1,11 @@
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
+const config = {
+	plugins: [
+		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
+		tailwindcss(),
+	]
+};
+
+module.exports = config;
